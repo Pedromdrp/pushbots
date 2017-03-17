7ce7f242a1073a62ddba0a767d3108f1073de6e2
Pedro da Rocha Pinto <pedromdrp@msn.com>
2017-03-17 16:39:10 +0000

    Update device.rb
    Added tags and alias.

--- lib/pushbots/device.rb
+++ lib/pushbots/device.rb
@@ -1,13 +1,15 @@
 module Pushbots
   # Device class
   class Device
-    attr_accessor :token, :platform, :tags
+    attr_accessor :token, :platform, :tags, :device_alias
     PLATFORM_TYPE = { ios: 0, android: 1, chrome: 2 }.freeze
     PLATFORM_TYPE_R = [:ios, :android, :chrome].freeze
 
-    def initialize(token, platform = nil)
+    def initialize(token, platform = nil, tags = nil, device_alias = nil)
       self.token = token
       self.platform = platform if platform
+      self.tags = tags if tags
+      self.device_alias = device_alias if device_alias
     end
 
     def info
@@ -16,11 +18,12 @@ module Pushbots
         http_response = JSON.parse(response)
         self.platform = PLATFORM_TYPE_R[http_response['platform']]
         self.tags = http_response['tags']
+        self.device_alias = http_response['alias']
       end
     end
 
     def register
-      body = { token: token, platform: PLATFORM_TYPE[platform] }
+      body = { token: token, platform: PLATFORM_TYPE[platform], tags: tags, alias: device_alias }
       response = Request.register(body)
       response.code == 201
     end
